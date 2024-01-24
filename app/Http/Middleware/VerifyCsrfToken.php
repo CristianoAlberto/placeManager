c1951bfd3d511719790f69ac6d4783149d658fa5
CristianoAlberto <cristianoalberto908347@gmail.com>
2024-01-24 15:37:11 +0100

    feat: eliminando uma rota

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -13,7 +13,6 @@ class VerifyCsrfToken extends Middleware
      */
     protected $except = [
         'createPlace',
-        'placeUpdate/*',
-        'deletePlace/*'
+        'placeUpdate/*'
     ];
 }
